9d028c9611765bd304f8059f140491e0bdbf248e
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2019-10-16 19:44:06 +0200

    need fix
    need fix

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 //slideshow
 var map = 'img/' //map
 var images = [map + '1.jpg', map + '2.jpg', map + '3.jpg']; //images
-var section = document.querySelector('.banner img');
-var interval = setInterval(slideshow, 2000);
+var section = document.querySelector('.banner img'); //query selector
+var interval = setInterval(slideshow, 2000); //interval
 
 
 
 
 function slideshow() {
-    var random = images[Math.floor(Math.random() * images.length)];
-    section.src = random;
+    var random = images[Math.floor(Math.random() * images.length)]; //pak random
+    section.src = random; //zet src in variable random
 
 }
