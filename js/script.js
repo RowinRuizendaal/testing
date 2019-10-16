019c0b69ee8829bb4fa1118410148a7e3a470e8e
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2019-10-16 19:33:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-console.log('test');
 
-var map = 'img/'
-var images = ['']
+//slideshow
+var map = 'img/' //map
+var images = [map + '1.jpg', map + '2.jpg', map + '3.jpg']; //images
+var section = document.querySelector('.banner img');
+var interval = setInterval(slideshow, 2000);
+
+
+
 
 function slideshow() {
+    var random = images[Math.floor(Math.random() * images.length)];
+    section.src = random;
 
 }
