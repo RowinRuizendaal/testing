b06c751249183308dd5560a24b02269c89f5a5b5
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2019-10-17 13:01:19 +0200

    new

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # testing
+https://stackoverflow.com/questions/22672368/how-to-make-a-transparent-html-button/22672444<br>
--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 var images = ['img/1.jpg', 'img/2.jpg','img/3.jpg']; //images
 var section = document.querySelector('.banner img'); //query selector
 var interval = setInterval(slideshow, 2000); //interval
+//End of slideshow
 
 
-
-
+//Function slideshow
 function slideshow() {
     var random = images[Math.floor(Math.random() * images.length)]; //pak random
     section.src = random; //zet src in variable random
 
 }
+
+//Funtion scroll into view when pressed on button
