a685e39a12df80884a29199f6b005a8c2f088ea4
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2019-10-23 19:04:30 +0200

    bijna klaar
    bijna klaar

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+let state = 'Show';
+var section = document.querySelector('.filter form');
+
+
+function slider() {
+
+
+    document.querySelector('#Value').textContent = '€ ' + document.querySelector('#prijsrange').value;
+}
+
+
+function Filter() {
+    section.classList.toggle('FormActive');
+
+
+    if (state == 'Hide') {
+        document.querySelector('.DuurzaamHeader button').innerHTML = 'Gebruik filter';
+        state = 'Show';
+    } else {
+        document.querySelector('.DuurzaamHeader button').innerHTML = 'Verberg filter';
+        state = 'Hide';
+    }
+    console.log(state);
+}
+
+
+
+
+document.querySelector('.DuurzaamHeader button').addEventListener('click', Filter);
+document.querySelector('#prijsrange').addEventListener('input', slider);
